81fc58ae61c10a707091f82aa4fadaf8b429c2ca
uflakey <134938478+uflakey@users.noreply.github.com>
2024-03-11 03:25:39 +0200

    fin

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 import contactsRouter from "./routes/contactsRouter.js";
+import authRouter from "./routes/authRouter.js";
+import { authenticate } from "./middlewares/authenticate.js";
 const app = express();
 
 const DB_KEY = process.env.DB_KEY;
@@ -20,7 +22,8 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", authenticate, contactsRouter);
+app.use("/api/users", authRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
