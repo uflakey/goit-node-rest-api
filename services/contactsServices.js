79bd0a5af2de67f3e89e721163bb47a55a12ed2b
uflakey <134938478+uflakey@users.noreply.github.com>
2024-03-20 03:02:35 +0200

    1111

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,21 +1,21 @@
 import { Contact } from "../models/contact.js";
 
-export async function listContacts() {
-  return Contact.find();
+export async function listContacts(owner) {
+  return Contact.find(owner);
 }
 
-export async function getContactById(contactId) {
-  return Contact.findById(contactId);
+export async function getContactById(_id, owner) {
+  return Contact.findOne({ _id, owner });
 }
 
-export async function removeContact(contactId) {
-  return Contact.findByIdAndDelete(contactId);
+export async function removeContact(_id, owner) {
+  return Contact.findOneAndDelete({ _id, owner });
 }
 
 export async function addContact(data) {
   return Contact.create(data);
 }
 
-export async function editContact(contactId, data) {
-  return Contact.findByIdAndUpdate(contactId, data, { new: true });
+export async function editContact(_id, data, owner) {
+  return Contact.findOneAndUpdate({ _id, owner }, data, { new: true });
 }
