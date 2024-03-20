b45a657749b89f2b812a925976fad4ed9a35669d
uflakey <134938478+uflakey@users.noreply.github.com>
2024-03-20 15:43:39 +0200

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,6 +4,7 @@ import {
   loginUser,
   logoutUser,
   currentUser,
+  avatarUser,
 } from "../controllers/authControllers.js";
 import validateBody from "../helpers/validateBody.js";
 import { authenticate } from "../middlewares/authenticate.js";
@@ -11,6 +12,7 @@ import {
   loginUserJoiSchema,
   registerUserJoiSchema,
 } from "../schemas/authSchemas.js";
+import { upload } from "../middlewares/upload.js";
 
 const authRouter = express.Router();
 
@@ -18,5 +20,6 @@ authRouter.post("/register", validateBody(registerUserJoiSchema), registerUser);
 authRouter.post("/login", validateBody(loginUserJoiSchema), loginUser);
 authRouter.get("/current", authenticate, currentUser);
 authRouter.post("/logout", authenticate, logoutUser);
+authRouter.patch("/avatars", authenticate, upload.single("avatar"), avatarUser);
 
 export default authRouter;
